b07a3c50cbd745795096a15592135e24caf54824
Zevrant <workmailtome@gmail.com>
2021-02-16 17:35:20 -0500

    FT_ZHS-30 initial commit

---
+++ src/main/java/com/zevrant/services/zevrantnotificationservice/ZevrantNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zevrant.services.zevrantnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZevrantNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZevrantNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zevrant/services/zevrantnotificationservice/ZevrantNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zevrant.services.zevrantnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZevrantNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
